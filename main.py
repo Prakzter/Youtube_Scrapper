2126517c5d5f9ddaece66a52811b88e508b43786
Prakash Nadarajan <prakash.akltg@gmail.com>
2020-07-07 12:05:29 +0800

    comment downloader works, able to print all comments , without replies

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import pandas as pd
 from selenium import webdriver
+import re
 import time
 
-
-driver = webdriver.Chrome("C:\webdrivers\chromedriver")
-yt_link = input("Link to Youtube video: ")
-print("-------------------------------------------------------------------------------")
-driver.get(yt_link)
-driver.maximize_window()
-time.sleep(5)
+# open browser and print video title
+driver = webdriver.Chrome("/Users/prakky/Desktop/DataSCIENCE/Git_proj/Youtube_Scrapper/chromedriver")
+driver.get("https://www.youtube.com/watch?v=ZtTt822bDNE")
+time.sleep(2)
 title = driver.find_element_by_xpath('//*[@id="container"]/h1/yt-formatted-string').text
 print("Video Title: " + title)
 print("------------------------------------------------------------------------------------")
 
+# page loading to retrieve all comments
 comment_section = driver.find_element_by_xpath('//*[@id="comments"]')
 driver.execute_script("arguments[0].scrollIntoView();", comment_section)
-time.sleep(7)
+time.sleep(2)
 
 last_height = driver.execute_script("return document.documentElement.scrollHeight")
 while True:
@@ -23,7 +22,7 @@ while True:
     driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
 
     # Wait to load page
-    time.sleep(2)
+    time.sleep(3)
 
     # Calculate new scroll height and compare with last scroll height
     new_height = driver.execute_script("return document.documentElement.scrollHeight")
@@ -32,30 +31,10 @@ while True:
     last_height = new_height
 
 driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
+  
 
-def remove_emoji(string):
-    emoji_pattern = re.compile("["
-                               u"\U0001F600-\U0001F64F"  # emoticons
-                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                               u"\U00002500-\U00002BEF"  # chinese char
-                               u"\U00002702-\U000027B0"
-                               u"\U00002702-\U000027B0"
-                               u"\U000024C2-\U0001F251"
-                               u"\U0001f926-\U0001f937"
-                               u"\U00010000-\U0010ffff"
-                               u"\u2640-\u2642"
-                               u"\u2600-\u2B55"
-                               u"\u200d"
-                               u"\u23cf"
-                               u"\u23e9"
-                               u"\u231a"
-                               u"\ufe0f"  # dingbats
-                               u"\u3030"
-                               "]+", flags=re.UNICODE)
-    return emoji_pattern.sub(r'', string)
 
+# printing out comments
 name_elems=driver.find_elements_by_xpath('//*[@id="author-text"]')
 comment_elems = driver.find_elements_by_xpath('//*[@id="content-text"]')
 num_of_names = len(name_elems)
@@ -68,4 +47,39 @@ for i in range(num_of_names):
     print(username + ": " + comment) 
     print("-------------------------------------------")
 
+print(str(num_of_names) + " comments in total")
+
 driver.close()
+
+
+
+
+
+
+
+
+# def remove_emoji(string):
+#     emoji_pattern = re.compile("["
+#                                u"\U0001F600-\U0001F64F"  # emoticons
+#                                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+#                                u"\U0001F680-\U0001F6FF"  # transport & map symbols
+#                                u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+#                                u"\U00002500-\U00002BEF"  # chinese char
+#                                u"\U00002702-\U000027B0"
+#                                u"\U00002702-\U000027B0"
+#                                u"\U000024C2-\U0001F251"
+#                                u"\U0001f926-\U0001f937"
+#                                u"\U00010000-\U0010ffff"
+#                                u"\u2640-\u2642"
+#                                u"\u2600-\u2B55"
+#                                u"\u200d"
+#                                u"\u23cf"
+#                                u"\u23e9"
+#                                u"\u231a"
+#                                u"\ufe0f"  # dingbats
+#                                u"\u3030"
+#                                "]+", flags=re.UNICODE)
+#     return emoji_pattern.sub(r'', string)
+
+# yt_link = input("Link to Youtube video: ")
+# print("-------------------------------------------------------------------------------")
